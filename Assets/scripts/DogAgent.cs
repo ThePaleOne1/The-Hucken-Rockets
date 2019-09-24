c80c9cfaea3aea431fa215a8c373bb0a1c989b97
Lachlan <mindcraftlachlan@outlook.com>
2019-09-24 10:20:43 +1000

    Implimenting main scene
    implementing grey box, player controller, and AI agent

---
+++ Assets/scripts/DogAgent.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DogAgent : MonoBehaviour
+{
+    //temportary variable just so i can test if the dog actually finds the target
+    public bool foundTarget;
+    //target that will constantly change, so the dog will walk towards sounds
+    public GameObject target;
+    //same as target but this will stay as the player so i can do other calculations
+    public GameObject Player;
+    //NavMesh Agent reference (fundemental to the AI system)
+    NavMeshAgent dogAgent;
+
+    [SerializeField]
+    private float detectionRange = 3f;
+
+
+    
+    void Start()
+    {
+        dogAgent = GetComponent<NavMeshAgent>();
+    }
+
+    
+    void Update()
+    {
+
+        if (foundTarget)//whatever triggure we want
+        {
+            dogAgent.destination = target.transform.position; //walk towards position
+        }
+
+        if (Vector3.Distance(transform.position, Player.transform.position) < detectionRange)
+        {
+            dogAgent.destination = Player.transform.position; //walk towards the player specifically
+        }
+
+    }
+}
