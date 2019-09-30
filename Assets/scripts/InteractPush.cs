a817054be3fcd8f685daf0b5959455eca1f728bc
Lachlan <mindcraftlachlan@outlook.com>
2019-09-30 10:03:16 +1000

    Added push script and tested with chair
    push interactions simply trigger a preset animation

---
+++ Assets/scripts/InteractPush.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractPush : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (!(other.gameObject.tag == "Player"))
+            return;
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            anim.SetBool("fallen", true);
+        }
+    }
+}
