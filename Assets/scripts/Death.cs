b621601b1b42eca69aecfb3d9dbf84a38780c330
zane <djzanman@gmail.com>
2019-10-15 11:02:26 +1100

    game over and particles

---
+++ Assets/scripts/Death.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour
+{
+    
+    
+    public GameObject confetti;
+    public GameObject deathScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Destroy(other.gameObject);
+            print("kill me bitch");
+            Instantiate(confetti, other.gameObject.transform.position,gameObject.transform.rotation);
+            
+        }
+            
+    }
+}
