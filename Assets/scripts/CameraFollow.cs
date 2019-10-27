5dc5d590226c6ed7960a9d05101e065d0bfb6a13
Lachlan <mindcraftlachlan@outlook.com>
2019-10-28 09:46:48 +1100

    camera fully funtional
    camera does not overshoot when the player walks too close to the edge of the map.
    also implemented sky box

--- Assets/scripts/CameraFollow.cs
+++ Assets/scripts/CameraFollow.cs
@@ -4,19 +4,34 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    
+
     public GameObject Player;
     public float height;
     public float distance;
+    public GameObject frontBarrier;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3 (Player.transform.position.x, Player.transform.position.y + height, Player.transform.position.z + distance);
+        transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y + height, transform.position.z);
+        if (transform.position.z > frontBarrier.transform.position.z)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, Player.transform.position.z + distance);
+        }
+
+
+        if (Player.transform.position.z - transform.position.z > Mathf.Abs(distance))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, Player.transform.position.z + distance);
+        }
+
+
+
+
     }
 }
