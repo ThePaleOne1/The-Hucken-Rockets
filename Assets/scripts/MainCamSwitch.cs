1a3afc6dc84dfe662a489261bc40223d5f205f7d
Lachlan <mindcraftlachlan@outlook.com>
2019-11-11 12:00:59 +1100

    seperated cutscene into different scene
    easier to playtest now pluss adding additional features such as  a skip cut scene is now implemented as it now allows for that

--- Assets/scripts/MainCamSwitch.cs
+++ Assets/scripts/MainCamSwitch.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainCamSwitch : MonoBehaviour
 {
@@ -8,27 +9,41 @@ public class MainCamSwitch : MonoBehaviour
     public GameObject CutsceneCam;
     public float delay;
     public RigidbodyCharacter pInput;
-	public GameObject CollectibleUI;
+
+    bool isSkippable = false;
+    
+	//public GameObject CollectibleUI;
 
     // Start is called before the first frame update
     void Start()
     {
         Invoke("SwitchCam", delay);
+        Invoke("Skippable", 2f);
         pInput.IsInputEnabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Space) && isSkippable)
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+
+    public void Skippable()
+    {
+        isSkippable = true;
     }
 
     public void SwitchCam()
     {
-        pInput.IsInputEnabled = true;
+        //pInput.IsInputEnabled = true;
         //print("yeeeett");
-        maincam.SetActive(true);
-        CutsceneCam.SetActive(false);
-		CollectibleUI.SetActive(true);
+        //maincam.SetActive(true);
+        //CutsceneCam.SetActive(false);
+        //CollectibleUI.SetActive(true);
+
+        SceneManager.LoadScene(2);
 	}
 }
