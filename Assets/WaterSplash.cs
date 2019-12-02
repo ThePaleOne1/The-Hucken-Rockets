0d52d4df2f6353e50dbd305c64a4a3bf41a48b72
Lachlan <mindcraftlachlan@outlook.com>
2019-12-03 10:37:37 +1100

    Fixed GUI buttons also water audio is in prorgess

---
+++ Assets/WaterSplash.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterSplash : MonoBehaviour
+{
+    public AudioClip Sploosh;
+    public AudioSource UAS;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+    
+    if (other.gameObject.tag == "Player")
+        {
+            UAS.PlayOneShot(Sploosh);
+        }
+    
+        
+    }
+}
