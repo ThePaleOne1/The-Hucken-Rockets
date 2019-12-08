bef90cac0c7594d89a7138fd7e4363ab61b40edd
Lachlan <mindcraftlachlan@outlook.com>
2019-12-09 09:37:08 +1100

    doors are closed in each section until you get the collectable

---
+++ Assets/scripts/DoorProgression.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorProgression : MonoBehaviour
+{
+    public Collectable colScript;
+    public int count;
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (colScript.collectableCount == count)
+        {
+            anim.SetBool("isOpen", true);
+        }
+        else
+        {
+            anim.SetBool("isOpen", false);
+        }
+    }
+}
